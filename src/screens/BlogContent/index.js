5990f4c422b326ffc5534a185527919f867f8852
alpertas <alpertas.cpp@gmail.com>
2023-05-16 21:31:30 +0300

    feat: initialized blog content page.

---
+++ src/screens/BlogContent/index.js
@@ -0,0 +1,13 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+// Styles
+import styles from './styles/BlogContentStyles';
+
+export default function BlogContent() {
+  return (
+    <View>
+      <Text>BlogContent</Text>
+    </View>
+  );
+}
