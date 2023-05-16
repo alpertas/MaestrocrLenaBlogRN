ebaba772e80816dec64d568c8027de8110512d51
alpertas <alpertas.cpp@gmail.com>
2023-05-16 21:24:03 +0300

    feat: initialized blog item component

---
+++ src/components/BlogItem/index.js
@@ -0,0 +1,13 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+// Styles
+import styles from './styles/BlogItemStyles';
+
+export default function BlogItem() {
+  return (
+    <View>
+      <Text style={styles.container}>index</Text>
+    </View>
+  );
+}
