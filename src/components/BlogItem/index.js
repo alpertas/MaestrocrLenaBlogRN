e3f5ecb2acada6fcfce67a476b5e1ed7e5e5fe04
alpertas <alpertas.cpp@gmail.com>
2023-05-17 14:46:06 +0300

    feat: changed blog item comp. and added navigator

--- src/components/BlogItem/index.js
+++ src/components/BlogItem/index.js
@@ -1,30 +1,29 @@
-import {View, Text, Image, StyleSheet} from 'react-native';
+import {View, Text, Image, TouchableOpacity} from 'react-native';
 import React from 'react';
 
 // Styles
 import styles from './styles/BlogItemStyles';
 
-export default function BlogItem() {
+export default function BlogItem({item, navigation}) {
   return (
-    <View style={styles.card}>
-      <View style={styles.cardHeader}>
-        <Image
-          source={{uri: 'https://source.unsplash.com/600x400/?computer'}}
-          style={styles.cardImage}
-          resizeMode="cover"
-        />
+    <TouchableOpacity
+      onPress={() => navigation.navigate('BlogContent', {postId: item.postId})}>
+      <View style={styles.card}>
+        <View style={styles.cardHeader}>
+          <Image
+            source={{uri: item.banner}}
+            style={styles.cardImage}
+            resizeMode="cover"
+          />
+        </View>
+        <View style={styles.cardBody}>
+          <Text style={styles.title}>{item.title}</Text>
+          <Text>{item.summary}</Text>
+        </View>
+        <View style={styles.cardFooter}>
+          <Text>{item.totalReadingTime} minutes read</Text>
+        </View>
       </View>
-      <View style={styles.cardBody}>
-        <Text style={styles.title}>What's new in 2022 Tech</Text>
-        <Text>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi
-          perferendis molestiae non nemo doloribus. Doloremque, nihil! At ea
-          atque quidem!
-        </Text>
-      </View>
-      <View style={styles.cardFooter}>
-        <Text>estimate time</Text>
-      </View>
-    </View>
+    </TouchableOpacity>
   );
 }
