0385d89cd41c065390460e4354bd4128b3e23ad1
alpertas <alpertas.cpp@gmail.com>
2023-05-17 14:44:29 +0300

    fix: added find method to find post id

--- src/screens/BlogContent/index.js
+++ src/screens/BlogContent/index.js
@@ -1,13 +1,19 @@
 import {View, Text} from 'react-native';
-import React from 'react';
+import React, {useContext} from 'react';
+import {BlogContext} from '../../contexts/BlogContext';
 
 // Styles
 import styles from './styles/BlogContentStyles';
 
-export default function BlogContent() {
+export default function BlogContent({route}) {
+  const {blogPosts} = useContext(BlogContext);
+  const {postId} = route.params;
+  const blogPost = blogPosts.find(post => post.postId === postId);
+
   return (
     <View>
-      <Text>BlogContent</Text>
+      <Text>{blogPost.title}</Text>
+      <Text>{blogPost.content}</Text>
     </View>
   );
 }
